d10505b1ac625c247b8fcf434ac264f8cb3d59a5
Nicolas Salvan <nicolas.salvan@etu.sorbonne-universite.fr>
2025-03-18 21:36:45 -0400

    corrected script

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-# Répertoires
-WORKING_DIR=/mti881-projet2  # Répertoire de travail
-OUTPUT_DIR=/output  # Répertoire de sortie
-DATA_DIR=/data  # Répertoire des données
-ERROR_DIR=/error  # Répertoire des erreurs
 
-# Paramètres du job
 #SBATCH --job-name=projet2-test-nico  # Nom du job
 #SBATCH --mem=1G  # Mémoire requise
 #SBATCH --gres=gpu:1  # Nombre de GPU requis - ne pas modifier !!!
-#SBATCH --time=7:00:00  # Temps d'exécution demandé (hh:mm:ss) - Ne pas dépasser 7h
-#SBATCH --chdir=$WORKING_DIR  # Répertoire de travail
-#SBATCH --output=$OUTPUT_DIR/%j_output.log  # Fichier de sortie
-#SBATCH --error=$ERROR_DIR/%j_error.log  # Fichier d'erreur
+#SBATCH --time=0:05:00  # Temps d'exécution demandé (hh:mm:ss) - Ne pas dépasser 7h
 
-# Préparation de l'environnement
+#SBATCH --chdir=/srv/nfs/logti-hyper-c1/au78760@ens.ad.etsmtl.ca/job/projet2-test-nico # Répertoire de travail
+
+#SBATCH --output=output/%j/output.log  # Fichier de sortie
+#SBATCH --error=$ERROR_DIR/%j/error.log  # Fichier d'erreur
+
+# Prepare environment
 nvidia-smi
-export HF_HOME=$WORKING_DIR/hfcache
-source $WORKING_DIR/venv/bin/activate
+gpu
+export HF_HOME=/srv/nfs/logti-hyper-c1/au78760@ens.ad.etsmtl.ca/cache
+source .venv/bin/activate
+pip install -r requirements.txt
 
-# Exécution du script
+# # Exécution du script
 python3 train_conll2003.py \
     --model_name_or_path bert-base-uncased \
     --dataset_name conll2003 \
-    --output_dir $OUTPUT_DIR/test-ner \
+    --output_dir test-ner \
     --do_train \
     --do_eval \
-    --trust_remote_code=True \
-    --data_dir $DATA_DIR \
-    --overwrite_output_dir
+    --trust_remote_code=True 
+
+# python3 test_import.py
