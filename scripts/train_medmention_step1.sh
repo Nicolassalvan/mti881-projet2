2041bf198a77933926acd46681724e49598554a0
Nicolas Salvan <nicolas.salvan@etu.sorbonne-universite.fr>
2025-04-02 11:54:18 -0400

    fixing val loss

--- scripts/train_medmention_step1.sh
+++ scripts/train_medmention_step1.sh
@@ -45,6 +45,10 @@ python3  ~/mti881-projet2/train_medmention.py \
     --eval_strategy=steps \
     --fp16 \
     --gradient_accumulation_steps=2 \
+    --learning_rate=1e-5 \
+    --per_device_train_batch_size=16 \
+    --per_device_eval_batch_size=16 \
+    --max_seq_length=128 \
     
 
 
