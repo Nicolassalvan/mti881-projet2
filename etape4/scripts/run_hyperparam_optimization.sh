8d24289ce061afee024048198eeac500b69d6b08
TessCza <131802027+TessCza@users.noreply.github.com>
2025-04-15 03:17:49 -0400

    Update run_hyperparam_optimization.sh

--- etape4/scripts/run_hyperparam_optimization.sh
+++ etape4/scripts/run_hyperparam_optimization.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=hyperparam-optuna  # Nom du job
-#SBATCH --mem=8G  # Mémoire requise
+#SBATCH --mem=8G  # MÃ©moire requise
 #SBATCH --gres=gpu:1  # Nombre de GPU requis - ne pas modifier !!!
-#SBATCH --time=07:00:00  # Temps d'exécution demandé (hh:mm:ss) - Ne pas dépasser 7h
-#SBATCH --chdir=../   # Répertoire de travail
+#SBATCH --time=07:00:00  # Temps d'exÃ©cution demandÃ© (hh:mm:ss) - Ne pas dÃ©passer 7h
+#SBATCH --chdir=../   # RÃ©pertoire de travail
 #SBATCH --output=job/slurm-%j.out  # Fichier de sortie
-#SBATCH --error=job/slurm-%j.err  # Fichier d'erreur
+#SBATCH --error=job/slurm-%j.err # Fichier d'erreur
 
 echo "Working directory: $(pwd)"
 echo "Starting at $(date)"
@@ -21,13 +21,13 @@ export TRANSFORMERS_CACHE=$HF_HOME
 mkdir -p $TRANSFORMERS_CACHE
 
 source ../venv/bin/activate
-JSON_PATH="./umls/dataset_concat.json"
+JSON_PATH="./dataset_concat.json"
 
 python3 ./hyperparam_optimization.py
 
 
 WEBHOOK_URL="https://discord.com/api/webhooks/1353477405624373289/-UN_D0e9qnOhK7lqVqEXLkQdCPTJn13bPNIrMn3kVRe5OfYapzS7kGN59Kn9y9mcjJcx"
-MESSAGE="Job termin� : $SLURM_JOB_NAME (ID: $SLURM_JOB_ID)"
+MESSAGE="Job terminé : $SLURM_JOB_NAME (ID: $SLURM_JOB_ID)"
 
 curl -H "Content-Type: application/json" \
      -X POST \
@@ -35,13 +35,13 @@ curl -H "Content-Type: application/json" \
      $WEBHOOK_URL
 
 # Analyse 
-echo "Analyse des r�sultats"
+echo "Analyse des résultats"
 
 python3 ~/mti881-projet2/analyse_metrics.py \
     --save_dir ~/mti881-projet2/etape4/figures/ \
     --checkpoint_dir ~/mti881-projet2/etape4/test-ner/ \
 
-# Envoi des r�sultats sur Discord
+# Envoi des résultats sur Discord
 python3 ~/mti881-projet2/send_discord.py \
     --webhook_url $WEBHOOK_URL \
     --img_dir ~/mti881-projet2/etape4/figures/ 
